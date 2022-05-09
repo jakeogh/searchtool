ea0a5cadc6d61c0ab0e4239267e5958003abff1f
jakeogh <github.com@v6y.net>
2022-05-08 21:39:52 -0700

    auto-commit

--- searchtool/searchtool.py
+++ searchtool/searchtool.py
@@ -20,78 +20,62 @@
 # pylint: disable=W0201  # attribute defined outside __init__
 # pylint: disable=R0916  # Too many boolean expressions in if statement
 # pylint: disable=C0305  # Trailing newlines editor should fix automatically, pointless warning
-# pylint: disable=C0413  # TEMP isort issue [wrong-import-position] Import "from pathlib import Path" should be placed at the top of the module [C0413]
 
-# code style:
-#   no guessing on spelling: never tmp_X always temporary_X
-#   dont_makedirs -> no_makedirs
-#   no guessing on case: local vars, functions and methods are lower case. classes are ThisClass(). Globals are THIS.
-#   del vars explicitely ASAP, assumptions are buggy
-#   rely on the compiler, code verbosity and explicitness can only be overruled by benchamrks (are really compiler bugs)
-#   no tabs. code must display the same independent of viewer
-#   no recursion, recursion is undecidiable, randomly bounded, and hard to reason about
-#   each elementis the same, no special cases for the first or last elemetnt:
-#       [1, 2, 3,] not [1, 2, 3]
-#       def this(*.
-#                a: bool,
-#                b: bool,
-#               ):
-#
-#   expicit loop control is better than while (condition):
-#       while True:
-#           # continue/break explicit logic
-#   only computer generated commit messages _should_ start with a cap letter
-
-
-# TODO:
-#   https://github.com/kvesteri/validators
+import asyncio
 import os
 import sys
-import click
 import time
-import sh
-from clicktool import click_add_options, click_global_options
-from click_auto_help import AHGroup
-from signal import signal, SIGPIPE, SIG_DFL
 from pathlib import Path
-#from with_sshfs import sshfs
-#from with_chdir import chdir
-from mptool import output
-from clicktool import tv
-from asserttool import validate_slice
-from eprint import eprint
-from asserttool import ic
-from retry_on_exception import retry_on_exception
-#from collections import defaultdict
-#from prettyprinter import cpprint
-#from prettyprinter import install_extras
-#install_extras(['attrs'])
-from timetool import get_timestamp
-#from configtool import click_read_config
-#from configtool import click_write_config_entry
-
-#from asserttool import not_root
-#from pathtool import path_is_block_special
-#from pathtool import write_line_to_file
-#from getdents import files
-#from prettytable import PrettyTable
-#output_table = PrettyTable()
-
-from mptool import unmp
-#from typing import List
-#from typing import Tuple
-from typing import Sequence
-#from typing import Generator
+from signal import SIG_DFL
+from signal import SIGPIPE
+from signal import signal
+# from typing import ByteString
+# from typing import Generator
+# from typing import List
+# from typing import Tuple
 from typing import Iterable
-#from typing import ByteString
 from typing import Optional
+from typing import Sequence
 from typing import Union
 
-sh.mv = None  # use sh.busybox('mv'), coreutils ignores stdin read errors
+import click
+# import sh
+from asserttool import ic
+# from asserttool import validate_slice
+from click_auto_help import AHGroup
+from clicktool import click_add_options
+from clicktool import click_global_options
+from clicktool import tv
+# from eprint import eprint
+# from with_sshfs import sshfs
+# from with_chdir import chdir
+from mptool import output
+from mptool import unmp
+from retry_on_exception import retry_on_exception
+from youtubesearchpython.__future__ import VideosSearch
+
+# from collections import defaultdict
+# from prettyprinter import cpprint
+# from prettyprinter import install_extras
+# install_extras(['attrs'])
+# from timetool import get_timestamp
+
+# from configtool import click_read_config
+# from configtool import click_write_config_entry
+
+# from asserttool import not_root
+# from pathtool import path_is_block_special
+# from pathtool import write_line_to_file
+# from getdents import files
+# from prettytable import PrettyTable
+# output_table = PrettyTable()
+
+
+# sh.mv = None  # use sh.busybox('mv'), coreutils ignores stdin read errors
 
 # click-command-tree
-#from click_plugins import with_plugins
-#from pkg_resources import iter_entry_points
+# from click_plugins import with_plugins
+# from pkg_resources import iter_entry_points
 
 # import pdb; pdb.set_trace()
 # #set_trace(term_size=(80, 24))
@@ -103,118 +87,95 @@ sh.mv = None  # use sh.busybox('mv'), coreutils ignores stdin read errors
 ##
 ##sys.excepthook = log_uncaught_exceptions
 
-#this should be earlier in the imports, but isort stops working
+# this should be earlier in the imports, but isort stops working
 signal(SIGPIPE, SIG_DFL)
 
 
-#@with_plugins(iter_entry_points('click_command_tree'))
-#@click.group(no_args_is_help=True, cls=AHGroup)
-#@click_add_options(click_global_options)
-#@click.pass_context
-#def cli(ctx,
-#        verbose: Union[bool, int, float],
-#        verbose_inf: bool,
-#        dict_input: bool,
-#        ) -> None:
-#
-#    tty, verbose = tv(ctx=ctx,
-#                      verbose=verbose,
-#                      verbose_inf=verbose_inf,
-#                      )
-
-
-# update setup.py if changing function name
-#@click.argument("slice_syntax", type=validate_slice, nargs=1)
-@click.command()
-@click.argument("paths", type=str, nargs=-1)
-@click.argument("sysskel",
-                type=click.Path(exists=False,
-                                dir_okay=True,
-                                file_okay=False,
-                                allow_dash=False,
-                                path_type=Path,),
-                nargs=1,
-                required=True,)
-@click.option('--ipython', is_flag=True)
-@click_add_options(click_global_options)
-@click.pass_context
-def cli(ctx,
-        paths: Sequence[str],
-        sysskel: Path,
-        ipython: bool,
-        verbose: Union[bool, int, float],
-        verbose_inf: bool,
-        dict_input: bool,
-        ) -> None:
-
-    tty, verbose = tv(ctx=ctx,
-                      verbose=verbose,
-                      verbose_inf=verbose_inf,
-                      )
-
-    if paths:
-        iterator = paths
-    else:
-        iterator = unmp(valid_types=[bytes,], verbose=verbose)
-    del paths
-
-    index = 0
-    for index, _path in enumerate(iterator):
-        path = Path(os.fsdecode(_path)).resolve()
-        if verbose:
-            ic(index, path)
-
-        with open(path, 'rb') as fh:
-            path_bytes_data = fh.read()
-
-        output(path, reason=None, dict_input=dict_input, tty=tty, verbose=verbose)
-
-#        if ipython:
-#            import IPython; IPython.embed()
-
-if __name__ == '__main__':
-    # pylint: disable=E1120
-    cli()
-
-#!/usr/bin/env python3
-
-from asserttool import ic
-import asyncio
-from youtubesearchpython.__future__ import VideosSearch
-#search = VideosSearch('NoCopyrightSounds')
-search = VideosSearch('zf6hp26')
-from mptool import output
-
-
-def print_result(result):
-    results = result['result']
-    ic(type(results))
-    ic(len(results))
+def print_result(
+    *,
+    result,
+    term: str,
+    tty: bool,
+    dict_input: bool,
+    verbose: Union[bool, int, float],
+):
+    results = result["result"]
+    # ic(type(results))
+    # ic(len(results))
     for _result in results:
-        ic(_result['link'], _result['title'])
+        # ic(_result["link"], _result["title"])
+        out_dict = {_result["title"]: _result["link"]}
+        output(out_dict, reason=term, dict_input=dict_input, tty=tty, verbose=verbose)
 
 
-async def main():
+async def main(
+    *, term: str, tty: bool, dict_input: bool, verbose: Union[bool, int, float]
+):
+    search = VideosSearch(term)
     while True:
         result = await search.next()
-        print_result(result)
+        print_result(
+            result=result, term=term, tty=tty, dict_input=dict_input, verbose=verbose
+        )
 
-    #result = await search.next()
-    #print_result(result)
+    # result = await search.next()
+    # print_result(result)
 
-    #result = await search.next()
-    #print_result(result)
 
-    #result = await search.next()
-    #print_result(result)
+@click.group(no_args_is_help=True, cls=AHGroup)
+@click_add_options(click_global_options)
+@click.pass_context
+def cli(
+    ctx,
+    verbose: Union[bool, int, float],
+    verbose_inf: bool,
+    dict_input: bool,
+) -> None:
 
-    #result = await search.next()
-    #print(result['result'])
+    tty, verbose = tv(
+        ctx=ctx,
+        verbose=verbose,
+        verbose_inf=verbose_inf,
+    )
 
-    #result = await search.next()
-    #print(result['result'])
 
-    #result = await search.next()
-    #print(result['result'])
+@click.command("youtube")
+@click.argument("terms", type=str, nargs=-1)
+@click_add_options(click_global_options)
+@click.pass_context
+def _youtube(
+    ctx,
+    terms: Sequence[str],
+    verbose: Union[bool, int, float],
+    verbose_inf: bool,
+    dict_input: bool,
+) -> None:
 
-asyncio.run(main())
+    tty, verbose = tv(
+        ctx=ctx,
+        verbose=verbose,
+        verbose_inf=verbose_inf,
+    )
+
+    if terms:
+        iterator = terms
+    else:
+        iterator = unmp(
+            valid_types=[
+                str,
+            ],
+            verbose=verbose,
+        )
+    del terms
+
+    index = 0
+    for index, _term in enumerate(iterator):
+        if verbose:
+            ic(index, _term)
+
+        asyncio.run(main(term=_term, tty=tty, dict_input=dict_input, verbose=verbose))
+
+
+if __name__ == "__main__":
+    # pylint: disable=E1120
+    cli()
